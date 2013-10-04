8692ee84e4270a9c1faa5b6d8fea6619e6d1db11
Eric Pierce <epierce@usf.edu>
2013-10-04 15:24:35 -0400

    Added debug logging to credential and Metadata Populator

--- src/main/java/edu/usf/cims/cas/support/janrain/authentication/JanrainAuthenticationMetaDataPopulator.java
+++ src/main/java/edu/usf/cims/cas/support/janrain/authentication/JanrainAuthenticationMetaDataPopulator.java
@@ -8,6 +8,9 @@ import org.jasig.cas.authentication.principal.Principal;
 import org.jasig.cas.authentication.principal.SimplePrincipal;
 import edu.usf.cims.cas.support.janrain.authentication.principal.JanrainCredentials;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 /**
  * This class is a meta data populator for authentication using Janrain Engage.
  *
@@ -16,15 +19,18 @@ import edu.usf.cims.cas.support.janrain.authentication.principal.JanrainCredenti
  */
 public final class JanrainAuthenticationMetaDataPopulator implements AuthenticationMetaDataPopulator {
 
+    private static final Logger LOGGER = LoggerFactory.getLogger(JanrainAuthenticationMetaDataPopulator.class);
+
     public Authentication populateAttributes(Authentication authentication, Credentials credentials) {
         if (credentials instanceof JanrainCredentials) {
           JanrainCredentials janrainCredentials = (JanrainCredentials) credentials;
-          final Principal simplePrincipal = new SimplePrincipal(authentication.getPrincipal().getId(),
-                                                                  janrainCredentials.getUserAttributes());
-            final MutableAuthentication mutableAuthentication = new MutableAuthentication(simplePrincipal,
-                                                                                          authentication
-                                                                                              .getAuthenticatedDate());
+          final Principal simplePrincipal = new SimplePrincipal(authentication.getPrincipal().getId(), janrainCredentials.getUserAttributes());
+            LOGGER.debug("Added Attributes [{}] to SimplePrincipal [{}]", janrainCredentials.getUserAttributes(), simplePrincipal);
+
+            //Create a new Authentication object with this principal
+            final MutableAuthentication mutableAuthentication = new MutableAuthentication(simplePrincipal, authentication.getAuthenticatedDate());
             mutableAuthentication.getAttributes().putAll(authentication.getAttributes());
+
             return mutableAuthentication;
         }
         return authentication;
--- src/main/java/edu/usf/cims/cas/support/janrain/authentication/principal/JanrainCredentials.java
+++ src/main/java/edu/usf/cims/cas/support/janrain/authentication/principal/JanrainCredentials.java
@@ -12,6 +12,9 @@ import org.apache.commons.lang.StringUtils;
 import org.jasig.cas.authentication.principal.Credentials;
 import com.googlecode.janrain4j.api.engage.response.profile.Profile;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 /**
  * This class creates a CAS-compatible credential using data from Janrain Engage
  *
@@ -21,6 +24,8 @@ import com.googlecode.janrain4j.api.engage.response.profile.Profile;
 public final class JanrainCredentials implements Credentials {
 
     private static final long serialVersionUID = 2749515040385101768L;
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(JanrainCredentials.class);
 
     /** The token that will sent to the Janrain user_info service */
     private String token;
@@ -62,23 +67,29 @@ public final class JanrainCredentials implements Credentials {
 
         if(userProfile.getProviderName() != null){
             userAttributes.put("ProviderName", userProfile.getProviderName());
+            LOGGER.debug("Set ProviderName: {}", userProfile.getProviderName());
         }
         if(userProfile.getPrimaryKey() != null){
             userAttributes.put("PrimaryKey", userProfile.getPrimaryKey());
+            LOGGER.debug("Set PrimaryKey: {}", userProfile.getPrimaryKey());
         }
         if(userProfile.getDisplayName() != null){
             userAttributes.put("DisplayName", userProfile.getDisplayName());
+            LOGGER.debug("Set DisplayName: {}", userProfile.getDisplayName());
         }
         if(userProfile.getName() != null) {
             if(userProfile.getName().getFamilyName() != null){
                 userAttributes.put("FamilyName", userProfile.getName().getFamilyName());
+                LOGGER.debug("Set FamilyName: {}", userProfile.getName().getFamilyName());
             }
             if(userProfile.getName().getGivenName() != null){
                 userAttributes.put("GivenName", userProfile.getName().getGivenName());
+                LOGGER.debug("Set GivenName: {}", userProfile.getName().getGivenName());
             }
         }
         if(userProfile.getBirthday() != null){
             userAttributes.put("Birthday", userProfile.getBirthday());
+            LOGGER.debug("Set Birthday: {}", userProfile.getBirthday());
         }
         /*
          * If the 'VerifiedEmail' attribute exists, use that as the user's email address.
@@ -86,43 +97,56 @@ public final class JanrainCredentials implements Credentials {
          */
         if(userProfile.getVerifiedEmail() != null){
             userAttributes.put("Email", userProfile.getVerifiedEmail());
+            LOGGER.debug("Set Email: {}", userProfile.getVerifiedEmail());
         } else if(userProfile.getEmail() != null){
             userAttributes.put("Email", userProfile.getEmail());
+            LOGGER.debug("Set Email: {}", userProfile.getEmail());
         }
         if(userProfile.getProviderName() != null){
             userAttributes.put("PhoneNumber", userProfile.getPhoneNumber());
+            LOGGER.debug("Set PhoneNumber: {}", userProfile.getPhoneNumber());
         }
         if(userProfile.getPreferredUsername() != null){
             userAttributes.put("PreferredUsername", userProfile.getPreferredUsername());
+            LOGGER.debug("Set PreferredUsername: {}", userProfile.getPreferredUsername());
         }
         if(userProfile.getPhoto() != null){
             userAttributes.put("PhotoURL", userProfile.getPhoto());
+            LOGGER.debug("Set PhotoURL: {}", userProfile.getPhoto());
         }
         if(userProfile.getUrl() != null){
             userAttributes.put("Url", userProfile.getUrl());
+            LOGGER.debug("Set Url: {}", userProfile.getUrl());
         }
         if(userProfile.getUtcOffset() != null){
             userAttributes.put("UTCoffset", userProfile.getUtcOffset());
+            LOGGER.debug("Set UTCoffset: {}", userProfile.getUtcOffset());
         }
         if(userProfile.getGender() != null){
             userAttributes.put("Gender", userProfile.getGender());
+            LOGGER.debug("Set Gender: {}", userProfile.getGender());
         }
         if(userProfile.getAddress() != null) {
             if(userProfile.getAddress().getCountry() != null){
                 userAttributes.put("Country", userProfile.getAddress().getCountry());
+                LOGGER.debug("Set Country: {}", userProfile.getAddress().getCountry());
             }
             if(userProfile.getAddress().getLocality() != null){
                 userAttributes.put("Locality", userProfile.getAddress().getLocality());
+                LOGGER.debug("Set Locality: {}", userProfile.getAddress().getLocality());
             }
             if(userProfile.getAddress().getPostalCode() != null){
                 userAttributes.put("PostalCode", userProfile.getAddress().getPostalCode());
+                LOGGER.debug("Set PostalCode: {}", userProfile.getAddress().getPostalCode());
             }
             if(userProfile.getAddress().getStreetAddress() != null){
                 userAttributes.put("StreetAddress", userProfile.getAddress().getStreetAddress());
+                LOGGER.debug("Set StreetAddress: {}", userProfile.getAddress().getStreetAddress());
             }
         }
         if(friendList.size() > 0){
             userAttributes.put("FriendList", friendList);
+            LOGGER.debug("Set FriendList: {}", friendList);
         }
         this.userAttributes = userAttributes;
     }
